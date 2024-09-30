421444975ddecfbbe79d39650b7e4b66aca4064b
yang4916 <park49164357@gmail.com>
2024-09-30 18:01:06 +0900

    test

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,14 +1,15 @@
 // src/components/Calendar.js
-import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-import './Calendar.css';
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import "./Calendar.css";
 
 const MyCalendar = () => {
   const [date, setDate] = useState(new Date());
 
   return (
     <div className="calendar-container">
+      <h1>수정 테스트</h1>
       <Calendar onChange={setDate} value={date} />
       <p>선택한 날짜: {date.toDateString()}</p>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css'; // 전역 CSS 임포트
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // 전역 CSS 임포트
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
