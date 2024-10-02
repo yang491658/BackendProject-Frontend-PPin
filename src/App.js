dab1952403918b783de10cb22004dd946ee713d2
yang4916 <park49164357@gmail.com>
2024-10-02 21:33:33 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import Dashboard from './pages/Dashboard';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
   return (
@@ -10,7 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        {/* 필요에 따라 다른 라우트를 추가하세요 */}
+        {/* 필요에 따라 다른 라우트를 추가하세요. */}
       </Routes>
     </Router>
   );
