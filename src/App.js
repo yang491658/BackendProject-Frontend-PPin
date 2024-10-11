8d1ba50724ffceeee40cdc0f7856a3b2f38e8ec2
yang4916 <park49164357@gmail.com>
2024-10-11 16:29:18 +0900

    asdf

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import Dashboard from './pages/Dashboard';
-import DocumentPage from './pages/DocumentPage'; // 문서함 페이지
-import Documents from './pages/Documents'; // 문서 작성 페이지
-import CalendarPage from './pages/CalendarPage';
-import MySalaryPage from './pages/MySalaryPage';
-import OrganizationChart from './pages/OrganizationChart';
-import SettingsPage from './pages/SettingPage';
-import AttendancePage from './pages/AttendancePage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
+import DocumentPage from "./pages/DocumentPage"; // 문서함 페이지
+import Documents from "./pages/Documents"; // 문서 작성 페이지
+import CalendarPage from "./pages/CalendarPage";
+import MySalaryPage from "./pages/MySalaryPage";
+import OrganizationChart from "./pages/OrganizationChart";
+import SettingsPage from "./pages/SettingPage";
+import AttendancePage from "./pages/AttendancePage";
+import DocumentDetailPage from "./pages/DocumentDetail";
 
-import './App.css'; 
+import "./App.css";
 
 const App = () => {
   return (
@@ -18,8 +19,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/documentpage" element={<DocumentPage />} /> {/* 문서함 페이지 */}
-        <Route path="/documents/create" element={<Documents />} /> {/* 문서 작성 페이지 */}
+        <Route path="/documentpage" element={<DocumentPage />} />{" "}
+        <Route path="/documents/create" element={<Documents />} />{" "}
+        <Route path="/documents/:dno" element={<DocumentDetailPage />} />{" "}
         <Route path="/mypay" element={<MySalaryPage />} />
         <Route path="/calendar" element={<CalendarPage />} />
         <Route path="/organization" element={<OrganizationChart />} />
